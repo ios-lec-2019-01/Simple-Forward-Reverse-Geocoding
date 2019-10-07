c4c30fdffa37d37b8ddaa3ca05899bdb60735409
Jonghyun Kim <jhkim@dit.ac.kr>
2019-10-07 13:32:43 +0900

    Initial Commit

---
+++ SimpleGeoCoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleGeoCoding
+//
+//  Created by amadeus on 07/10/2019.
+//  Copyright © 2019 DIT Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
